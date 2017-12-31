77bec6e4162ff508f44e522828368a57e1afef4b
Zaid <zaid.naom@gmail.com>
2017-12-31 02:37:46 +0100

    Add non-safe SimpleJson.parse method

--- src/SimpleJson.fs
+++ src/SimpleJson.fs
@@ -5,6 +5,12 @@ open Parser
 open AST
 
 module SimpleJson = 
-    /// Tries to parse a string into a Json structure
+    /// Tries to parse a string into a Json structured JSON data.
     let tryParse (input: string) : Option<Json> = 
         Parsimmon.parse input jsonParser
+
+    /// Parses the input string into a structured JSON data. Fails with an exception if parsing fails. 
+    let parse (input: string) : Json = 
+        match tryParse input with
+        | Some result -> result
+        | None -> failwithf "Could not parse the JSON input: %s" input
